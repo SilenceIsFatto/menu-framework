b2d75d77a88387b2bc30bb890fa2bc74a6acb0ca
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-02-17 15:13:27 +0000

    add mod.cpp

---
+++ menu_framework/mod.cpp
@@ -0,0 +1,16 @@
+// #if __has_include("\z\menu_framework\addons\core\script_component.hpp")
+//     #include "\x\A3A\addons\core\script_component.hpp"
+//     name = QUOTE(Main Menu Framework);
+// #endif
+
+name = "Main Menu Framework";
+author = "Silence";
+description = "";
+tooltip = "Main Menu Framework";
+tooltipOwned = "Main Menu Framework";
+overview = "A framework for modifying the main menu screen of Arma 3";
+actionName = "GitHub";
+action = "https://github.com/SilenceIsFatto/menu-framework";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = {1, 0, 0, 1};
